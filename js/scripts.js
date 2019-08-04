2a4cc31f138b88f13248ae306102c18fe2bcf9e2
claudinos <claudinos1990@gmail.com>
2019-08-04 20:51:48 +0200

    add js of contructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 
+var totalPriceArray = [];
+function order(size, toppings, crust, number) {
+  this.customSize = size;
+  this.crust = crust;
+  this.Topping = toppings;
+  this.number = number;
+}
+order.prototype.pizzaCost = function () {
+  return this.customSize + " " + this.crust + " " + this.Topping + " " + this.number
+}
+
